8bacf5a8ac009f4463ea8a865420d68be3206946
sandy <sandyks.lo27@gmail.com>
2021-11-29 18:32:55 -0800

    added fetch login and fixed login route

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
 import Home from "./Home";
+import Login from "./Login";
+import UserProfile from "./UserProfile";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_confirmation, setPasswordConfirmation] = useState("");
   
 
   return (
     <div className="App">
+      <NavBar setUser={setUser} />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/me" element={<UserProfile />} />
+        <Route path="/login" element={<Login setUser={setUser}/>} />
       </Routes>
     </div>
   );
