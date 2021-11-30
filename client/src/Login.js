63b59819de69cfec8972fdaffcbd09c543f6a7e8
sandy <sandyks.lo27@gmail.com>
2021-11-29 18:37:03 -0800

    added comment to update main

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function Login({ setUser }) { // hello
+function Login({ setUser }) { // hello help
     const navigate = useNavigate();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
