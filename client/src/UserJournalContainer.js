c18b31233ec9df67f1e7613eb6b4f0c1f92c811c
sandy <sandyks.lo27@gmail.com>
2021-12-07 11:44:18 -0800

    fixed user refresh on entries

--- client/src/UserJournalContainer.js
+++ client/src/UserJournalContainer.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import UserJournal from './UserJournal';
 
-function UserJournalContainer({ journals, user }) {
-    // const [userJournals, setUserJournals] = useState([]);
-
-    // console.log('checking user data', user)
-    // useEffect(() => {
-    //     fetch(`/users/${user.id}`)
-    //         .then((r) => r.json())
-    //         .then((data) => checkUserJournalData(data))
-    // }, []);
-
-    // function checkUserJournalData(data) {
-    //     if (!!data.journals) {
-    //         setUserJournals(data.journals);
-    //     }
-    // }
-
+function UserJournalContainer({ user }) {
     function renderUserJournals() {
         console.log(user);
-        return user.journals.map((userJournal) => {
+        return user?.journals.map((userJournal) => { // optional-chaining
             return (
                 <UserJournal 
                     key={userJournal.id}
