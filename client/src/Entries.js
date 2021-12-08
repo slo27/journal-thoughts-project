f5af40ca58833d7254231df1af8a1f6998161598
sandy <sandyks.lo27@gmail.com>
2021-12-07 17:18:45 -0800

    entries in middle css

--- client/src/Entries.js
+++ client/src/Entries.js
@@ -4,7 +4,7 @@ function Entries({ journals, user }) {
     return (
         <div>
             <div className="row justify-content-center">
-                <div className="col">
+                <div className="col-md-7">
                     <UserJournalContainer journals={journals} user={user} />
                 </div>
             </div>
